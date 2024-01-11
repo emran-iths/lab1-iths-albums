b13628cb18a271286698d67a611fd3b051d2b85a
emran-iths <155668324+emran-iths@users.noreply.github.com>
2024-01-11 12:34:13 +0100

    made album form (name is only field for now). form adds album to array. page also renders list of albums

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,50 @@
+import React, {useState} from 'react';
+
+const AlbumForm = ({onSubmit}) => {
+
+    const [name, setName] = useState('');
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSubmit({name: name});
+        setName('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={name} onChange={handleNameChange} />
+            <button type="submit">Submit</button>
+        </form>
+    );
+        
+}
+
 export default function Home() {
+
+  let default_albums = [
+    {"name":"test-album"}
+  ];
+
+  const [albums, setAlbums] = useState(default_albums);
+
   return (
     <>
-        empty app
+        <AlbumForm onSubmit={ (data) => {
+            console.log("form was submit", data); 
+            setAlbums( albums.concat( data));
+
+        } }/>
+        <ul>
+        { albums.map(  (album) => {
+            return <li key={album.name}>{album.name}</li>
+        })}
+        </ul>
+
+
     </>
   )
 }
