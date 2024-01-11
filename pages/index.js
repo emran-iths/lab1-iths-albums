e5067de5081c02a2b54ea22f83bb1bfc377acacd
emran-iths <155668324+emran-iths@users.noreply.github.com>
2024-01-11 14:01:30 +0100

    more views handler. can now goto a edit album view (view not done yet)

--- pages/index.js
+++ pages/index.js
@@ -36,25 +36,50 @@ export default function Home() {
 
   return (
     <>
-        { view == 'list' ? 
+        { view == 'list' ? // Comment examinator: I am aware that there is such as thing as a Switch statement in JS but since the iths document described this way of handling conditional rendering I went with what the documentation said. (I see nothing about what the correct way to do this according the requirement for this task)
+
 
         <>
+        <h2>List</h2>
         <button onClick={ () => { setView('create')  }  }>Add album</button>
 
         <ul>
         { albums.map(  (album) => {
-            return <li key={album.name}>{album.name}</li>
+            return <li key={album.name}>
+                    {album.name} 
+                    <button onClick={ () => { setView('edit')} }>Edit</button>
+                </li>
+            
         })}
         </ul>
         </>
 
+        :
+
+        view == 'edit' ? 
+
+        <>
+        <h2>Edit</h2>
+        <u></u>
+        </>
+
         :  
 
+        view == 'create' ?
+        <>
+        <h2>Create</h2>
         <AlbumForm onSubmit={ (data) => {
             console.log("form was submit", data); 
             setAlbums( albums.concat( data));
             setView('list');
         } }/>
+        </>
+
+        :
+
+        <>
+        Invalid view: {view}
+        </>
 
         }
 
